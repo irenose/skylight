98d9c52932b7578c8c4076d24e85ed2fd0e78555
Jack Sharkey <jsharkey@wrayward.com>
2014-12-03 11:16:43 -0500

    built out static residential skylights page (desktop)

--- application/modules/page/views/products/category.php
+++ application/modules/page/views/products/category.php
@@ -1,4 +1,4 @@
-<?php 
+<!-- <?php 
 	/******************************* CAROUSEL AND INTRO COPY *************************/ 
 ?>
 <section>
@@ -30,4 +30,128 @@
 		//Display Product Listing
 		echo $listing_content;
 	}
-?>
+?> -->
+
+<div class="breadcrumb">
+	<nav class="nav-breadcrumb">
+		<a>Installer Name</a> / <a>Our Products</a> / Product Category
+	</nav>
+	<div class="breadcrumb-links">
+		<a>Schedule a Consultation</a>
+		<a><span class="dotted">Have Questions or Comments?</span></a>
+	</div>
+</div>
+<section class="page-row page-row--tall hero hero--residential">
+	<div class="row">
+		<div class="small-12 large-6 columns">
+			<div class="vcard">
+				<h3>Residential Skylights</h3>
+				<p>VELUX residential skylights are a great way to add natural light and fresh air to your home. They not only improve your living space, but they also help improve energy efficiency.</p>
+			</div>
+		</div>
+	</div>
+</section>
+<?=$this->load->view('partials/_navigation-secondary')?>
+<section class="page-row page-row--squeezed">
+	<div class="header-statement">
+		<h3>Solar Powered "Fresh Air" Skylights</h3>
+	</div>
+	<div class="row product-row">
+		<div class="small-12 medium-6 columns centered">
+			<a href="" class="product-image"><img src="<?=asset_url('images/solar-powered-1.jpg')?>" alt></a>
+			<a class="product-title">Solar Powered "Fresh Air" Skylight (VCS)</a>
+			<p>Curb mounted skylight</p>
+			<img src="<?=asset_url('images/stars.png')?>" alt>
+		</div>
+		<div class="small-12 medium-6 columns centered">
+			<a href="" class="product-image"><img src="<?=asset_url('images/solar-powered-2.jpg')?>" alt></a>
+			<a class="product-title">Solar Powered "Fresh Air" Skylight (VSS)</a>
+			<p>Deck mounted skylight</p>
+			<img src="<?=asset_url('images/stars.png')?>" alt>
+		</div>
+	</div>
+</section>
+<section class="border-top-grey page-row page-row--squeezed">
+	<div class="header-statement">
+		<h3>Solar Powered "Fresh Air" Skylights</h3>
+	</div>
+	<div class="row product-row">
+		<div class="small-12 medium-6 columns centered">
+			<a href="" class="product-image"><img src="<?=asset_url('images/solar-powered-1.jpg')?>" alt></a>
+			<a class="product-title">Solar Powered "Fresh Air" Skylight (VCS)</a>
+			<p>Curb mounted skylight</p>
+			<img src="<?=asset_url('images/stars.png')?>" alt>
+		</div>
+		<div class="small-12 medium-6 columns centered">
+			<a href="" class="product-image"><img src="<?=asset_url('images/solar-powered-2.jpg')?>" alt></a>
+			<a class="product-title">Solar Powered "Fresh Air" Skylight (VSS)</a>
+			<p>Deck mounted skylight</p>
+			<img src="<?=asset_url('images/stars.png')?>" alt>
+		</div>
+	</div>
+</section>
+<section class="border-top-grey page-row page-row--squeezed">
+	<div class="header-statement">
+		<h3>Solar Powered "Fresh Air" Skylights</h3>
+	</div>
+	<div class="row product-row">
+		<div class="small-12 medium-6 columns centered">
+			<a href="" class="product-image"><img src="<?=asset_url('images/solar-powered-1.jpg')?>" alt></a>
+			<a class="product-title">Solar Powered "Fresh Air" Skylight (VCS)</a>
+			<p>Curb mounted skylight</p>
+			<img src="<?=asset_url('images/stars.png')?>" alt>
+		</div>
+		<div class="small-12 medium-6 columns centered">
+			<a href="" class="product-image"><img src="<?=asset_url('images/solar-powered-2.jpg')?>" alt></a>
+			<a class="product-title">Solar Powered "Fresh Air" Skylight (VSS)</a>
+			<p>Deck mounted skylight</p>
+			<img src="<?=asset_url('images/stars.png')?>" alt>
+		</div>
+	</div>
+</section>
+<section class="border-top-grey page-row page-row--squeezed">
+	<div class="header-statement">
+		<h3>Solar Powered "Fresh Air" Skylights</h3>
+	</div>
+	<div class="row product-row">
+		<div class="small-12 medium-6 columns centered">
+			<a href="" class="product-image"><img src="<?=asset_url('images/solar-powered-1.jpg')?>" alt></a>
+			<a class="product-title">Solar Powered "Fresh Air" Skylight (VCS)</a>
+			<p>Curb mounted skylight</p>
+			<img src="<?=asset_url('images/stars.png')?>" alt>
+		</div>
+		<div class="small-12 medium-6 columns centered">
+			<a href="" class="product-image"><img src="<?=asset_url('images/solar-powered-2.jpg')?>" alt></a>
+			<a class="product-title">Solar Powered "Fresh Air" Skylight (VSS)</a>
+			<p>Deck mounted skylight</p>
+			<img src="<?=asset_url('images/stars.png')?>" alt>
+		</div>
+	</div>
+</section>
+<section class="page-row page-row--snug">
+    <div class="header-statement">
+        <h3>Accessories</h3>
+    </div>
+    <div class="row accessories">
+        <div class="small-12 medium-6 large-3 columns">
+            <a href=""><img src="<?=asset_url('images/blinds.png')?>" alt></a>
+            <h5>Blinds</h5>
+            <a href="">Learn More</a>
+        </div>
+        <div class="small-12 medium-6 large-3 columns">
+            <a href=""><img src="<?=asset_url('images/glass.png')?>" alt></a>
+            <h5>Clean, Quiet &amp; Safe Glass</h5>
+            <a href="">Learn More</a>
+        </div>
+        <div class="small-12 medium-6 large-3 columns">
+            <a href=""><img src="<?=asset_url('images/remotes.png')?>" alt></a>
+            <h5>Remote Controls</h5>
+            <a href="">Learn More</a>
+        </div>
+        <div class="small-12 medium-6 large-3 columns">
+            <a href=""><img src="<?=asset_url('images/flashing.png')?>" alt></a>
+            <h5>Flashing</h5>
+            <a href="">Learn More</a>
+        </div>
+    </div>
+</section>
