d552fed54b8da7c5a77d412b5072ad28c0ce53b1
Geoff Parish <gparish@wrayward.com>
2014-12-18 16:51:12 -0500

    Added dynamic product category listings

--- application/modules/page/views/products/index.php
+++ application/modules/page/views/products/index.php
@@ -16,72 +16,36 @@
     ----------------------------------------------*/
 ?>
     <div class="product-category-wrapper">
-        <!-- <?php
-        	if( isset($product_category_array) && count($product_category_array) > 0) {
-        		foreach($product_category_array as $category) {
-        			echo '<a href="' . $installer_base_url . '/products/category/' . $category->product_category_url . '">' . $category->product_category_name . '</a><br>';
-        			echo filter_page_content($category->product_category_teaser) . '<br><br>';
-        		}
-        	}
-        ?> -->
+        <?php
+            if( isset($product_category_array) && count($product_category_array) > 0) {
+                $count = 0;
+                echo '<div class="row">' . "\n";
+                    foreach($product_category_array as $category) {
+                        $count++;
+                        switch(count($product_category_array)) {
+                            case 2:
+                                $class = $count == 1 ? 'small-12 medium-6 large-4 large-push-2 columns product-category' : 'small-12 medium-6 large-4 large-pull-2 columns product-category';
+                                break;
+                            case 3:
+                                $class = 'small-12 medium-4 columns product-category';
+                                break;
 
-        <div class="row">
-            <!-- <div class="small-12 medium-4 columns product-category">
-                <a href="">
-                    <div class="polaroid">
-                        <img src="<?=asset_url('images/residential-skylights.jpg')?>" class="desktop" alt>
-                        <img src="<?=asset_url('images/residential-skylights-short.jpg')?>" class="desktop-down" alt>
-                    </div>
-                </a>
-                <h5>Residential Skylights</h5>
-                <p>VELUX residential skylights are a great way to add natural light and fresh air to your home. They not only improve your living space, but they also help improve energy efficiency.</p>
-                <a class="btn">Learn More</a>
-            </div>
-            <div class="small-12 medium-4 columns product-category">
-                <a href="">
-                    <div class="polaroid">
-                        <img src="<?=asset_url('images/sun-tunnel-skylights.jpg')?>" class="desktop" alt>
-                        <img src="<?=asset_url('images/sun-tunnel-skylights-short.jpg')?>" class="desktop-down" alt>
-                    </div>
-                </a>
-                <h5>SUN TUNNEL Skylights</h5>
-                <p>If you don't want a sky view or have a small space that needs natural light, choose a fixed skylight or a SUN TUNNEL™ skylight.</p>
-                <a class="btn">Learn More</a>
-            </div>
-            <div class="small-12 medium-4 columns product-category">
-                <a href="">
-                    <div class="polaroid">
-                        <img src="<?=asset_url('images/commercial-skylights.jpg')?>" class="desktop" alt>
-                        <img src="<?=asset_url('images/commercial-skylights-short.jpg')?>" class="desktop-down" alt>
-                    </div>
-                </a>
-                <h5>Commercial Skylights</h5>
-                <p>VELUX commercial skylights not only improve energy efficiency, but they also provide optimal lighting and fresh air to enhance your buildings architectural design and performance.</p>
-                <a class="btn">Learn More</a>
-            </div> -->
-            <div class="small-12 medium-6 large-4 large-push-2 columns product-category">
-                <a href="">
-                    <div class="polaroid">
-                        <img src="<?=asset_url('images/residential-skylights.jpg')?>" class="desktop" alt>
-                        <img src="<?=asset_url('images/residential-skylights-short.jpg')?>" class="desktop-down" alt>
-                    </div>
-                </a>
-                <h5>Residential Skylights</h5>
-                <p>VELUX residential skylights are a great way to add natural light and fresh air to your home. They not only improve your living space, but they also help improve energy efficiency.</p>
-                <a class="btn">Learn More</a>
-            </div>
-            <div class="small-12 medium-6 large-4 large-pull-2 columns product-category">
-                <a href="">
-                    <div class="polaroid">
-                        <img src="<?=asset_url('images/commercial-skylights.jpg')?>" class="desktop" alt>
-                        <img src="<?=asset_url('images/commercial-skylights-short.jpg')?>" class="desktop-down" alt>
-                    </div>
-                </a>
-                <h5>Commercial Skylights</h5>
-                <p>VELUX commercial skylights not only improve energy efficiency, but they also provide optimal lighting and fresh air to enhance your buildings architectural design and performance.</p>
-                <a class="btn">Learn More</a>
-            </div>
-        </div>
+                        }
+                        echo '<div class="' . $class . '">' . "\n";
+                            echo '<a href="' . $installer_base_url . '/products/category/' . $category->product_category_url . '">' . "\n";
+                                echo '<div class="polaroid">' . "\n";
+                                    echo '<img src="' . asset_url('images/' . $category->category_image . '.jpg') . '" alt="' . $category->product_category_name . '" class="desktop">' . "\n";
+                                    echo '<img src="' . asset_url('images/' . $category->category_image . '-short.jpg') . '" alt="' . $category->product_category_name . '" class="desktop-down">' . "\n";
+                                echo '</div>' . "\n";
+                            echo '</a>' . "\n";
+                            echo '<h5>' . $category->product_category_name . '</h5>' . "\n";
+                            echo '<p>' . filter_page_content($category->product_category_teaser) . '</p>' . "\n";
+                            echo '<a class="btn" href="' . $installer_base_url . '/products/category/' . $category->product_category_url . '">Learn More</a>';
+                        echo '</div>' . "\n";
+                    }
+                echo '</div>' . "\n";
+            }
+        ?>
     </div>
 </section>
 <?php
