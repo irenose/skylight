2bf7141189b42545a900b1044bd1cf6a75fa555c
Jack Sharkey <jsharkey@wrayward.com>
2014-12-23 11:09:20 -0500

    updated sub-nav in header; removed tabs and replaced with spaces in installer and warranty pages

--- application/modules/page/views/home/installer.php
+++ application/modules/page/views/home/installer.php
@@ -23,24 +23,24 @@
 ?>
     <header class="intro-statement intro-statement--squeezed">
         <h2 class="normal-weight"><?=$installer_array[0]->about_dealer_headline?></h2>
-	</header>
+    </header>
 <?php
     /*---------------------------------------------
         End Intro Copy Headline
     ----------------------------------------------*/
 ?>
-	<div class="row installer-welcome">
-		<div class="small-12 large-6 columns welcome-hero upgrade">
-			<h1 class="reversed upper">Upgrade Your<br/>Home With An<br/>Upward View.</h1>
-			<a href="" class="btn">Learn More</a>
-		</div>
-		<div class="small-12 large-6 columns featured-images">
-			<div class="promotion-large fresh-air">
+    <div class="row installer-welcome">
+        <div class="small-12 large-6 columns welcome-hero upgrade">
+            <h1 class="reversed upper">Upgrade Your<br/>Home With An<br/>Upward View.</h1>
+            <a href="" class="btn">Learn More</a>
+        </div>
+        <div class="small-12 large-6 columns featured-images">
+            <div class="promotion-large fresh-air">
                 <h5 class="normal-weight">Solar Powered “Fresh Air” Skylight</h5>
                 <a href="">Learn More</a>
                 <div class="incentive"><span class="big">30%</span><br>Federal Tax<br>Credit</div>
-			</div>
-		</div>
+            </div>
+        </div>
         <div class="promotions-small">
             <div class="promotion-small reversed dealer-promo">
                 <h3 class="normal-weight">Save 20%</h3>
@@ -72,17 +72,17 @@
 ?>
         <section class="page-row border-top-grey">
         	<div class="testimonial-carousel header-statement">
-        		<div class="slick">
-        			<div class="slick-list">
-        				<div class="testimonial">
-        					<p><?=filter_page_content($testimonials_array[0]->testimonial_copy)?></p>
-        				</div>
-        			</div>
-        			<div class="testimonial-link">
-        				<a href="<?=$installer_base_url?>/about#testimonials">View All Testimonials</a>
-        			</div>
-        		</div>
-        	</div>
+                <div class="slick">
+                    <div class="slick-list">
+                        <div class="testimonial">
+                            <p><?=filter_page_content($testimonials_array[0]->testimonial_copy)?></p>
+                        </div>
+                    </div>
+                    <div class="testimonial-link">
+                        <a href="<?=$installer_base_url?>/about#testimonials">View All Testimonials</a>
+                    </div>
+                </div>
+            </div>
         </section>
 <?php
     }
--- application/modules/page/views/warranty.php
+++ application/modules/page/views/warranty.php
@@ -1,54 +1,54 @@
 <?php 
-	/******************************* INTRO COPY *************************/ 
+    /******************************* INTRO COPY *************************/ 
 ?>
 <section class="page-row intro-statement intro-statement--squeezed">
-	<h2 class="normal-weight">Warranty</h2>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate.</p>
+    <h2 class="normal-weight">Warranty</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate.</p>
 </section>
 
 <?php 
-	/******************************* 10 YEAR WARRANTY *************************/ 
+    /******************************* 10 YEAR WARRANTY *************************/ 
 ?>
-<section class="bg-grey-dark page-row reversed">
-	<h2 class="normal-weight">The 10 Year Warranty</h2>
-	<p class="text-columns-2 ten-year-warranty">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. <br><br>Beatae, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate.</p>
-	<a class="btn">Download PDF</a>
+<section class="page-row bg-grey-dark reversed 10-year-warranty">
+    <h2 class="normal-weight">The 10 Year Warranty</h2>
+    <p class="text-columns-2 ten-year-warranty">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. <br><br>Beatae, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt accusamus itaque deleniti iusto, doloribus eligendi et, voluptas ea. Beatae, voluptate.</p>
+    <a class="btn">Download PDF</a>
 </section>
 
 <?php
-	/******************************* OPTIONAL INSTALLER WARRANTY *************************/ 
-	if( isset($warranty_array) && count($warranty_array) > 0) {
+    /******************************* OPTIONAL INSTALLER WARRANTY *************************/ 
+    if( isset($warranty_array) && count($warranty_array) > 0) {
 ?>
 <?php
     /*---------------------------------------------
         Installer Warranty
     ----------------------------------------------*/
 ?>
-	<section class="page-row">
-		<h2 class="normal-weight"><?= $installer_array[0]->name; ?> Warranty</h2>
-		<p class="text-columns-2 installer-warranty"><?= filter_page_content($warranty_array[0]->dealer_warranty); ?></p>
-	</section>
+    <section class="page-row installer-warranty">
+        <h2 class="normal-weight"><?= $installer_array[0]->name; ?> Warranty</h2>
+        <p class="text-columns-2 installer-warranty"><?= filter_page_content($warranty_array[0]->dealer_warranty); ?></p>
+    </section>
 <?php
     /*---------------------------------------------
         End Installer Warranty
     ----------------------------------------------*/
 ?>
 <?php
-	}
+    }
 ?>
 
 <!-- <section>
-	<h2>Our Products</h2>
-	<?php
-		//SHOW PRODUCT CATEGORIES THAT DEALER OFFERS
-		foreach($product_category_array as $category) {
-			echo '<a href="' . $installer_base_url . '/products/category/' . $category->product_category_url . '">' . $category->product_category_name . '</a><br>';
-		}
-	?>
+    <h2>Our Products</h2>
+    <?php
+        //SHOW PRODUCT CATEGORIES THAT DEALER OFFERS
+        foreach($product_category_array as $category) {
+            echo '<a href="' . $installer_base_url . '/products/category/' . $category->product_category_url . '">' . $category->product_category_name . '</a><br>';
+        }
+    ?>
 </section> -->
 
 <?php 
-	/******************************* OUR PRODUCTS *************************/ 
+    /******************************* OUR PRODUCTS *************************/ 
 ?>
 <div class="border-top-grey"></div>
 <?=( $this->uri->segment(1) != 'styleguide' ? $this->load->view('partials/_products-short') : null );?>
