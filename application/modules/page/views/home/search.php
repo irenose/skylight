948c84eb01c00a7d6ce47c561aff9463b08216f5
Geoff Parish <gparish@wrayward.com>
2015-01-20 15:42:13 -0500

    Additional layout updates to main page

--- application/modules/page/views/home/search.php
+++ application/modules/page/views/home/search.php
@@ -6,6 +6,13 @@
 	<input type="submit" value="Find Installers">
 </form>
 -->
+<section class="page-row page-row--extra-tall hero hero--residential intro-statement">
+	<h1 class="reversed upper mega-heading">Bring the benefits of daylight home</h1>
+</section>
+<section class="page-row intro-statement intro-statement--squeezed">
+	<h2>Bringing daylight to life</h2>
+	<p>For over 60 years, VELUX has designed energy-efficient daylighting solutions for commercial and residential buildings. By letting more daylight into your home, VELUX skylights help you use one of our most abundant natural resources, daylight, to illuminate and add drama any room.</p>
+</section>
 <?php
 	$data['cta_type'] = 'short';
     echo $this->load->view('partials/_find-installer', $data);
