1904cd7ce46881f11224b98666dddd19900cd5b6
Geoff Parish <gparish@wrayward.com>
2015-01-27 16:02:14 -0500

    Copy change from proofing

--- application/modules/page/views/home/search.php
+++ application/modules/page/views/home/search.php
@@ -14,7 +14,7 @@
 <section class="page-row">
 	<header class="intro-statement intro-statement--squeezed bringing-daylight">
 		<h2>Bringing daylight to life</h2>
-		<p>For over 60 years, VELUX has designed energy-efficient daylighting solutions for commercial and residential buildings. By letting more daylight into your home, VELUX skylights help you use one of our most abundant natural resources, daylight, to illuminate and add drama any room.</p>
+		<p>For over 60 years, VELUX has designed energy-efficient daylighting solutions for commercial and residential buildings. By letting more daylight into your home, VELUX skylights help you use one of our most abundant natural resources, daylight, to illuminate and add drama to any room.</p>
 	</header>
 </section>
 
