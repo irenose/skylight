3185622e67cd3869a2af6ac62691afbabad789db
Geoff Parish <gparish@wrayward.com>
2015-01-29 13:22:18 -0500

    Updated thank you copy

--- application/modules/page/views/contact/thanks.php
+++ application/modules/page/views/contact/thanks.php
@@ -1,13 +1,13 @@
 <section class="page-row bg-grey promotions">
     <header class="intro-statement intro-statement--squeezed">
-    	<?php
-    		if(isset($form_status) && $form_status == 'success') {
-    			echo '<h1>Thank You</h1>';
-    			echo '<p>Thank you copy goes here</p>';
-    		} else {
-    			echo '<h1>Error</h1>';
-    			echo '<p>There was a problem. Call us at ' . $installer_array[0]->phone1 . '</p>';
-    		}
-    	?>
+        <?php
+            if(isset($form_status) && $form_status == 'success') {
+                echo '<h1>Thank you for your request.</h1>';
+                echo '<p>We will be in touch shortly.</p>';
+            } else {
+                echo '<h1>Error</h1>';
+                echo '<p>There was a problem with your submission. Please call us at ' . $installer_array[0]->phone1 . ' to discuss your request.</p>';
+            }
+        ?>
     </header>
 </section>
