1fb18882860bc0a54a3a19c9bf548b00c05530b1
Jack Sharkey <jsharkey@wrayward.com>
2015-01-30 15:48:35 -0500

    updating heading weight - now normal weight instead of bold - due to browser rendering of montserrat font (bold is too wide)

--- application/modules/page/views/why_skylights.php
+++ application/modules/page/views/why_skylights.php
@@ -21,7 +21,7 @@
         </div>
         <div class="small-12 medium-6 columns reason">
             <span class="reason__number">1</span>
-            <h2>Everything under the sun looks better.</h2>
+            <h2 class="normal-weight">Everything under the sun looks better.</h2>
             <p>Because skylights provide balanced, natural light from above, colors appear truer. Why paint your walls or spend time getting your home’s palette just right, when you can’t see the colors as they’re meant to be seen? Unlike windows, which provide daylight that may be very bright or dull depending on the time of day, skylights provide balanced daylight that fills even remote corners of a room.</p>
         </div>
     </div>
@@ -39,7 +39,7 @@
         </div>
         <div class="small-12 medium-6 medium-pull-6 columns reason">
             <span class="reason__number">2</span>
-            <h2>Skylights create a healthier environment.</h2>
+            <h2 class="normal-weight">Skylights create a healthier environment.</h2>
             <p>No one wants to smell last night’s fish dinner. Nor do you want to breathe chemicals released by synthetic materials, such as carpet, paint, wall coverings and upholstered furniture that make up modern homes.</p>
         </div>
     </div>
@@ -57,7 +57,7 @@
         </div>
         <div class="small-12 medium-6 columns reason">
             <span class="reason__number">3</span>
-            <h2>Skylights help reduce dependence on air conditioning.</h2>
+            <h2 class="normal-weight">Skylights help reduce dependence on air conditioning.</h2>
             <p>When you have skylights that can open installed in your home, you can use them to draw cool, fresh air inside instead of turning on the air conditioner. Skylights use the chimney effect when opened in conjunction with windows on a lower level of the home. Hot air naturally rises and is pulled out of the skylights, while cool, fresh air is pulled in through lower level windows.</p>
         </div>
     </div>
@@ -75,7 +75,7 @@
         </div>
         <div class="small-12 medium-6 medium-pull-6 columns reason">
             <span class="reason__number">4</span>
-            <h2>Skylights provide daylight and a view without sacrificing privacy.</h2>
+            <h2 class="normal-weight">Skylights provide daylight and a view without sacrificing privacy.</h2>
             <p>With homes built close together windows and bathrooms and bedrooms often spend their days with draperies or shade pulled shut. With skylights you can have the light you need without fearing prying eyes.</p>
         </div>
     </div>
@@ -93,7 +93,7 @@
         </div>
         <div class="small-12 medium-6 columns reason">
             <span class="reason__number">5</span>
-            <h2>Skylights with blinds give you the ultimate in light control.</h2>
+            <h2 class="normal-weight">Skylights with blinds give you the ultimate in light control.</h2>
             <p>Modern skylights come with blinds or shades that allow homeowners to diffuse or block the light during certain times of the day. And VELUX skylights come with a programmable touch-screen remote control that allows homeowners to schedule times for the blinds or shades to close and open.</p>
         </div>
     </div>
@@ -111,7 +111,7 @@
         </div>
         <div class="small-12 medium-6 medium-pull-6 columns reason">
             <span class="reason__number">6</span>
-            <h2>As we age our eyes require more light for everyday tasks.</h2>
+            <h2 class="normal-weight">As we age our eyes require more light for everyday tasks.</h2>
             <p>As you age, the lenses of your eyes thicken and the pupils shrink in size, causing your eye to adapt much more slowly to changing light conditions. Researchers McFarland and Fisher reported that to accommodate the adaptation of the aging eye, the amount of light required for visual acuity doubled for each 13 years after the age of 20. Natural light from above provides soft, balanced light ideal for eyes of all ages.</p>
         </div>
     </div>
@@ -129,7 +129,7 @@
         </div>
         <div class="small-12 medium-6 columns reason">
             <span class="reason__number">7</span>
-            <h2>In kitchens, using skylights for natural light and fresh air frees up wall space for additional cabinets.</h2>
+            <h2 class="normal-weight">In kitchens, using skylights for natural light and fresh air frees up wall space for additional cabinets.</h2>
             <p>A lack of storage is a common complaint about kitchens. With skylights, you get the natural light and fresh air your need in the kitchen, leaving room along the walls for additional storage.</p>
         </div>
     </div>
@@ -147,7 +147,7 @@
         </div>
         <div class="small-12 medium-6 medium-pull-6 columns reason">
             <span class="reason__number">8</span>
-            <h2>See your room in a whole new light every hour of the day.</h2>
+            <h2 class="normal-weight">See your room in a whole new light every hour of the day.</h2>
             <p>“Fresh Air” skylights open to bring air as well as natural light into your home. A rain sensor on the solar and electric models automatically closes the skylight at the first sign of rain.</p>
         </div>
     </div>
@@ -165,7 +165,7 @@
         </div>
         <div class="small-12 medium-6 columns reason">
             <span class="reason__number">9</span>
-            <h2>Skylights and roof windows are the ideal way to add natural light to bonus rooms.</h2>
+            <h2 class="normal-weight">Skylights and roof windows are the ideal way to add natural light to bonus rooms.</h2>
             <p>If you’re thinking about converting your home’s bonus room into a home office, play room or bedroom, skylights or roof windows are the perfect way to add light to the room.</p>
         </div>
     </div>
@@ -183,7 +183,7 @@
         </div>
         <div class="small-12 medium-6 medium-pull-6 columns reason">
             <span class="reason__number">10</span>
-            <h2>VELUX skylights give you peace of mind.</h2>
+            <h2 class="normal-weight">VELUX skylights give you peace of mind.</h2>
             <p>VELUX skylights come with a rain sensor that automatically closes the skylight at the first sign of rain. They are installed with three layers of weather protection – a pre-installed deck seal, all-weather underlayment and engineered step flashing – that prevent air and water from entering your home.</p>
         </div>
     </div>
