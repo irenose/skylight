b26e9e27259a1e4d95c5474baa7dca64779264bd
Geoff Parish <gparish@wrayward.com>
2015-01-31 20:32:48 -0500

    Removed CTA link from promotion clickthrough page b/c it wasn't supposed to be there

--- application/modules/page/views/promotions.php
+++ application/modules/page/views/promotions.php
@@ -2,6 +2,5 @@
     <header class="intro-statement intro-statement--squeezed">
         <h1><?= $installer_array[0]->promotion_headline; ?></h1>
         <p><?= filter_page_content($installer_array[0]->promotion_page_copy); ?></p>
-        <a href="" class="cta-text">CTA</a>
     </header>
 </section>
