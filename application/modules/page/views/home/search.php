94a3b35296dcd0464d7682c3cfd2ade19257d7d5
Geoff Parish <gparish@wrayward.com>
2015-02-05 17:14:00 -0500

    Changed main page headlines to normal-weight

--- application/modules/page/views/home/search.php
+++ application/modules/page/views/home/search.php
@@ -8,12 +8,12 @@
 -->
 <section class="page-row page-row--extra-tall hero hero-search">
 	<header class="intro-statement">
-		<h1 class="reversed upper mega-heading">Bring the benefits of daylight home</h1>
+		<h1 class="reversed upper mega-heading normal-weight">Bring the benefits of daylight home</h1>
 	</header>
 </section>
 <section class="page-row">
 	<header class="intro-statement intro-statement--squeezed bringing-daylight">
-		<h2>Bringing daylight to life</h2>
+		<h2 class="normal-weight">Bringing daylight to life</h2>
 		<p>For over 60 years, VELUX has designed energy-efficient daylighting solutions for commercial and residential buildings. By letting more daylight into your home, VELUX skylights help you use one of our most abundant natural resources, daylight, to illuminate and add drama to any room.</p>
 	</header>
 </section>
