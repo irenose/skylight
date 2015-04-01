7f67bde9d2bf5b0cc46cc560aed6a1adb6fa828c
Geoff Parish <gparish@wrayward.com>
2015-04-01 13:25:35 -0400

    Generic review page setup

---
+++ application/modules/page/views/submit_review.php
@@ -0,0 +1,10 @@
+<?=$this->load->view('partials/_bz-javascript-init')?>
+<script>
+	$BV.ui('rr', 'submit_generic', {});
+</script>
+<section class="page-row bg-grey promotions">
+    <header class="intro-statement intro-statement--squeezed">
+        <p>Own a VELUX skylight? Write a review!</p>
+    </header>
+</section>
+<?=$this->load->view('partials/_footer--search.php');?>
